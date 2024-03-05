ce1c2e0817cfa7a3a37858765e63569e994e7d0b
adibhauzan <adibhauzan48@gmail.com>
2024-03-06 01:38:18 +0700

    configure portfolio app for deployment to github Pages

---
+++ src/data/constants.js
@@ -0,0 +1,262 @@
+export const Bio = {
+  name: "Adib Hauzan Sofyan",
+  roles: [
+    "Backend Developer",
+    "DevOps Engineer",
+    "Fullstack Developer",
+  ],
+  description:
+    "Fresh Graduate students at the University of Logistics and International Business major in informatics engineering. Want to be able to work as a team to complete projects and be able to solve problems with the team. Interested in programming and learning new things. Committed to achieving project goals with strong programming skills and the ability to adapt to new technologies. With a background in IT, I possess specialized skills in programming using Golang and PHP. Proficient in managing both SQL and NoSQL databases, I also have expertise in using Docker for efficient containerization processes. I have a combination of academic proficiency, practical experience in the IT field, and specific skills in backend development, showcasing a competitive and dynamic professional in this field.",
+  github: "https://github.com/adibhauzan",
+  resume:
+    "https://drive.google.com/file/d/123iJ7S3j-LE7HY4_JDH0D6HbgPPNLRHE/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/adib-hauzan-3b7772254/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/adibhauzan_/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Typescript",
+        image:
+          "https://icon.icepanel.io/Technology/svg/TypeScript.svg",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwindcss",
+        image:
+        "https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600",
+      },
+      // {
+      //   name: "Flutter",
+      //   image:
+      //     "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      // },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Go-Lang",
+        image: "https://www.vectorlogo.zone/logos/golang/golang-official.svg",
+      },
+      {
+        name: "Laravel",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-original.svg",
+      },
+      {
+        name: "PHP",
+        image: "https://www.vectorlogo.zone/logos/php/php-ar21.svg",
+      },
+      
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+        "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original-wordmark.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Goland",
+        image:
+          "https://icon.icepanel.io/Technology/svg/GoLand.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://icon.icepanel.io/Technology/svg/Postman.svg",
+      },
+      {
+        name: "Linux",
+        image: "https://www.vectorlogo.zone/logos/linux/linux-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQMgyF-2jhRE0LcEYukNlxxRAFYdth2cQFUSR6pFRkjQ&s",
+    role: "Backend Developer",
+    company: "Shoeworkshop",
+    date: "Sep 2023 - Nov 2023",
+    desc: "As a backend developer at Shoeworkshop, my responsibilities include building service-backed solutions using the Laravel framework for the company's website or blog, as well as using the Go programming language for assistant applications. My tasks involve designing scalable systems, managing data processing, ensuring seamless communication between frontend and backend, collaborating with cross-functional teams, and optimizing backend performance to drive technological advancement.",
+    skills: [
+      "Docker",
+      "Laravel",
+      "Golang - Gin Framework",
+      "Postgresql",
+      "Mysql",
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://perpustakaan.mgi.esdm.go.id/template/default/img/logo.png",
+    role: "Staff IT Intern",
+    company: "Balai Besar Survei Dan Pemetaan Geologi Kelautan (BBSPGL)",
+    date: "June 2022 - March 2023",
+    desc: "As an IT staff member at the BBSPGL, my responsibility involves building, maintaining, and optimizing company applications using various programming languages, engaging in the entire software development life cycle, and collaborating with the IT team and campus departments to ensure information technology solutions meet security standards and institutional requirements, ultimately enhancing operational efficiency and user experience in the higher education environment.",
+    skills: [
+      "PHP - Laravel",
+      "NodeJs",
+      "Tailwindcss",
+      "MySql Database",
+      "JavaScript",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/optical-net-343607.appspot.com/o/channels4_profile.jpg?alt=media&token=8827b9a7-3143-47f4-8ea0-ec6204616c0f",
+    school: "Universitas Logistik dan Bisnis International, Bandung",
+    date: "Oct 2020 - Nov 2023",
+    // grade: "8.71 CGPA",
+    desc: "As a D3 Informatics Engineering graduate from the International Logistics and Business University with a GPA of 3.28, I possess a strong grasp of IT fundamentals, including programming and system analysis. My education, coupled with practical exposure to IT in business contexts, equips me to excel in software development, data analysis, IT management, and technology consulting roles. I offer valuable skills in comprehending and applying IT principles effectively to drive operational efficiency and strategic initiatives within organizations. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Diploma of Technology - Diploma 3, Informatics Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Shoeworkshop - Company Profile and Blog",
+    date: "Jul 2023 - Aug 2023",
+    description: "The Company Profile and Blog application for Shoeworkshop is a web platform that presents information about Shoeworkshop and allows the company to share content and the latest news through a blog. Built using the monolith approach with the Laravel framework, this application provides a responsive and secure interface, enabling users to explore the company's profile pages and read the latest news through the blog. The application also comes with a content management system that facilitates administrators in managing page content and blog articles.",
+    image: "https://firebasestorage.googleapis.com/v0/b/peaceful-impact-403104.appspot.com/o/shoeworkshop.png?alt=media&token=e6ff3635-908b-4dc0-a2b7-eb42afbac642" 
+      ,
+    tags: [
+      "Docker",
+      "PHP - Laravel",
+      "MySQL Database",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "https://shoeworkshop.id/",
+    member: [
+      {
+        name: "Adib Hauzan Sofyan  Backend Developer",
+        img: "https://avatars.githubusercontent.com/adibhauzan",
+        linkedin: "https://www.linkedin.com/in/adib-hauzan-3b7772254/",
+        github: "https://github.com/adibhauzan/",
+      },
+      {
+        name: "Ahmad Haris  Frontend Developer",
+        img: "https://avatars.githubusercontent.com/harisaris",
+        linkedin: "https://id.linkedin.com/in/ahmad-haris-370ab4137/",
+        github: "https://github.com/harisaris",
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "JPO - ERP System",
+    date: "Jul 2023 - Aug 2023",
+    description: "JPO (Online Bookkeeping Service) is an ERP system tailored specifically for the textile industry. It is designed to streamline inventory management, production processes, order tracking, financial management, and data analytics. By focusing on these key areas, JPO aims to enhance efficiency and responsiveness within textile companies. While still in the development phase, we are dedicated to further improving its functionality prior to the official launch, by carefully considering and incorporating feedback from beta users. We sincerely appreciate your ongoing support.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/peaceful-impact-403104.appspot.com/o/jpo.png?alt=media&token=78b33a84-f5dd-4e78-93cb-a792bf20ff59",
+    tags: [
+      "Laravel",
+      "Docker",
+      "MySQL Database",
+      "Vanilla Js",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "https://bachtiar21.github.io/jpo_erp_adm/login.html",
+    member: [
+     {
+        name: "Adib Hauzan Sofyan  Backend Developer",
+        img: "https://avatars.githubusercontent.com/adibhauzan",
+        linkedin: "https://www.linkedin.com/in/adib-hauzan-3b7772254/",
+        github: "https://github.com/adibhauzan/",
+      },
+      {
+        name: "Bachtiar Ramadhan  Frontend Developer",
+        img: "https://avatars.githubusercontent.com/Bachtiar21",
+        linkedin: "#",
+        github: "https://github.com/Bachtiar21",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
